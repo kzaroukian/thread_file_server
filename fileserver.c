70143704d587363a11f7c679b221864f5413ac8a
Kaylin Zaroukian <zaroukik@eos13.cis.gvsu.edu>
2019-02-07 09:18:37 -0500

    kaylin

--- fileserver.c
+++ fileserver.c
@@ -5,8 +5,35 @@
 #include <stdlib.h> 
 #include <string.h>
 
+void* find_file(void* arg);
+
 int main() {
 
+   char fileName[500];
+   printf("Enter a file name: ");
+   fgets(fileName,500,stdin);
+   // malloc for file name
+   int status;
+   pthread_t child;
+
+   if ((status = pthread_create(&child, NULL, find_file, &fileName)) != 0) {
+      fprintf(stderr, "Error creating thread %d: %s\n", status, strerror(status));
+      exit(1);
+   }
+
+   // otherwise thread was successfully created
+
    return 0;
+}
+
+void* find_file(void* arg) {
+   // free file name
+   int prob =  rand() % (101 - 1) + 1;
+
+   if (prob <= 20) {
+      sleep(1);
+   } else {
+      int sleep = rand() % ()   
+   }
 }
 
