ee330672111e5a176f869825d7fc2f10214b97ec
Marc Chesebro <Marc.chesebro@gmail.com>
2019-02-07 09:46:14 -0500

    mutex added

--- fileserver.c
+++ fileserver.c
@@ -10,6 +10,11 @@ void* serviceFile(void* arg);
 void interuptHandler(int sigNum);
 
 int numFile = 0;
+//mutex to avoid confliting writes
+pthread_mutex_t fileMutex = PTHREAD_MUTEX_INITIALIZER;
+
+int totalTime = 0;
+pthread_mutex_t timeMutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main() {
 
@@ -29,7 +34,18 @@ void* serviceFile(void* arg){
 	char* fileName = (char*)arg;
 	sleep(1);
 	printf("processing file %s", fileName);
+
+	//lock the fileMutex before incrementing the numFile variable to avoid race condition
+	pthread_mutex_lock(&fileMutex);
 	numFile++;
+	pthread_mutex_unlock(&fileMutex);
+
+	//do the same for the totalTime
+	pthread_mutex_lock(&timeMutex);
+	totalTime = totalTime + 2;
+	pthread_mutex_unlock(&timeMutex);
+
+	//free the filename
 	free(fileName);
 	return NULL;
 }
@@ -37,6 +53,24 @@ void* serviceFile(void* arg){
 void interuptHandler (int sigNum) 
 { 
 	printf ("received an interrupt.\n");
-	printf("total number of files processed: %d\n", numFile);	
+	//lock mutex
+	pthread_mutex_lock(&timeMutex);
+	pthread_mutex_lock(&fileMutex);
+
+	printf("total number of files processed: %d\n", numFile);
+	float averageTime = 0.0;
+	//avoid / by 0 errors
+	if (numFile != 0){
+		
+		averageTime = totalTime / numFile;
+
+
+	}
+	printf("averge file process time: %f\n", averageTime);
+
+	//unlock mutex
+	pthread_mutex_unlock(&fileMutex);
+	pthread_mutex_unlock(&timeMutex);
+	
 	exit(0); 
 }
