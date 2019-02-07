b4a81328d5373907711f320db85a666188e8ff83
Marc Chesebro <Marc.chesebro@gmail.com>
2019-02-07 09:18:13 -0500

    most of the creating thread logic

--- fileserver.c
+++ fileserver.c
@@ -5,8 +5,28 @@
 #include <stdlib.h> 
 #include <string.h>
 
-int main() {
+void* serviceFile(void* arg);
 
-   return 0;
+int numFile = 0;
+
+int main() {
+	
+	while(1){
+		//malloc to create new memory for each thread to avoid race condition
+		char* fileName = malloc(sizeof(char) * 50);
+		fgets(fileName, 50, stdin);
+		pthread_t thread;
+		pthread_create(&thread, NULL, serviceFile, fileName);
+	}
+	return 0;
+}
+
+void* serviceFile(void* arg){
+	char* fileName = (char*)arg;
+	sleep(1);
+	printf("processing file %s", fileName);
+	numFile++;
+	free(fileName);
+	return NULL;
 }
 
